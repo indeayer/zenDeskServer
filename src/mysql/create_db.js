15ba1d2130cc687bff35d1e565b3a86d15fd3f97
indeayer <indeayer@gmail.com>
2020-09-10 17:31:52 +0800

    Edited Git Ignore

---
+++ src/mysql/create_db.js
@@ -0,0 +1,44 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "ZenAdmin",
+  password: "@*ePEk3SwiYu&=oVuPhi"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  /*Create a database named "mydb":*/
+  con.query("CREATE DATABASE IF NOT EXISTS TESTTEST", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+
+  let useDB = "USE TESTTEST";
+
+  con.query(useDB, function(err, results, fields) {
+    if (err) {
+    console.log(err.message);
+    }
+});
+
+    
+    let createTodos = `create table if not exists userdata(
+        id int primary key auto_increment,
+        user_name varchar(100)not null,
+        first_name varchar(100)not null,
+        last_name varchar(100)not null
+    )`;
+    con.query(createTodos, function(err, results, fields) {
+        if (err) {
+        console.log(err.message);
+        }
+    });
+
+    con.end(function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+      });
+});
