2d69f67882d888aa2fd558f99780832203e84801
indeayer <indeayer@gmail.com>
2020-09-14 13:13:40 +0800

    updated sudo

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -13,5 +13,5 @@ sudo chkconfig docker on
 
 # Docker Compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 printf '\nDocker Compose installed successfully\n\n'
