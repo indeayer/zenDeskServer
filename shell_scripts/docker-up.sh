e16171fbe5bce49f12efe95828aedb0203e6b2e6
indeayer <indeayer@gmail.com>
2020-09-15 02:20:26 +0800

    update shell

--- shell_scripts/docker-up.sh
+++ shell_scripts/docker-up.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 cd ..
+cd src
+npm install
+printf '\Installing Local NPM Environment\n\n'
+sleep 5
+printf '\Booting docker-compose up..\n\n'
+sleep 5
+cd ..
 docker-compose up
 printf '\nDocker Compose up successfully\n\n'
